381cc98ad437b71fec645d02cc532fd145bae48e
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2018-10-08 18:22:27 +0900

    sample

--- GoogleAuth/public/js/config.js
+++ GoogleAuth/public/js/config.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 // https://console.firebase.google.com/project/YOUR_PROJECT_NAME/settings/general/
 var config = {
-    apiKey: "",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-    projectId: "YOUR_PROJECT",
-    storageBucket: "YOUR_PROJECT.appspot.com",
+    apiKey: "YOUR_API_ID",
+    authDomain: "test-auth-d37cd.firebaseapp.com",
+    databaseURL: "https://test-auth-d37cd.firebaseio.com",
+    projectId: "test-auth-d37cd",
+    storageBucket: "test-auth-d37cd.appspot.com",
     messagingSenderId: "YOUR_ID"
 };
 firebase.initializeApp(config);
